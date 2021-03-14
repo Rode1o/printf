674cefaa712a555e8cb8a45f7ffb3387eef8e8ca
jblanco75 <jblanco75@gmail.com>
2021-03-14 06:40:26 +0000

    update

--- holberton.h
+++ holberton.h
@@ -1,6 +1,16 @@
 #ifndef _PRINTF_
 #define _PRINTF_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <limits.h>
+
+
 int _printf(const char *format, ...);
+void printstring(char *s);
+void printinteger(int n);
+int _putchar(char c);
 
 #endif
